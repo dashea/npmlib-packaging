fcaba11081c5d994670485d0b4ea075b27b63232
David Shea <dshea@redhat.com>
2018-11-30 11:37:57 -0500

    eslint-ify the test scripts

--- test/test_provides.js
+++ test/test_provides.js
@@ -1,47 +1,49 @@
+/* eslint-env mocha */
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
+
 const tmp = require('tmp');
+
 tmp.setGracefulCleanup();
 
 const prov = require('../rpm/npmlib.prov');
 
 function makeModulesDir() {
-  var tmpdir = tmp.dirSync();
-  var modulesDir = path.join(tmpdir.name, 'node_modules');
+  const tmpdir = tmp.dirSync();
+  const modulesDir = path.join(tmpdir.name, 'node_modules');
   fs.mkdirSync(modulesDir);
   return modulesDir;
 }
 
-describe('auto-provides', function () {
-  it('should return the name and version from a package.json', function() {
+describe('auto-provides', () => {
+  it('should return the name and version from a package.json', () => {
     const testData = {
-      name: "test-package",
-      version: "1.2.3"
+      name: 'test-package',
+      version: '1.2.3',
     };
-    assert.equal(prov.processPackageJSON(testData), "npmlib(test-package) = 1.2.3\n");
-
+    assert.equal(prov.processPackageJSON(testData), 'npmlib(test-package) = 1.2.3\n');
   });
 });
 
-describe('bundled provides', function() {
-  it('should detect bundled dependencies', function () {
-    var modulesDir = makeModulesDir();
+describe('bundled provides', () => {
+  it('should detect bundled dependencies', () => {
+    const modulesDir = makeModulesDir();
     fs.mkdirSync(path.join(modulesDir, 'test-module'));
     fs.writeFileSync(path.join(modulesDir, 'test-module', 'package.json'),
-      JSON.stringify({name: 'test-module', version: '3.4.5'}));
+      JSON.stringify({ name: 'test-module', version: '3.4.5' }));
 
     assert.equal(prov.processBundledDeps(path.join(modulesDir, '..')),
       'bundled(npmlib(test-module)) = 3.4.5\n');
   });
 
-  it('should skip modules with no dependencies', function() {
-    var moduleDir = tmp.dirSync();
+  it('should skip modules with no dependencies', () => {
+    const moduleDir = tmp.dirSync();
     assert.equal(prov.processBundledDeps(moduleDir.name), '');
   });
 
-  it ('should skip non-bundled dependencies', function() {
-    var moduleDir = makeModulesDir();
+  it('should skip non-bundled dependencies', () => {
+    const moduleDir = makeModulesDir();
     fs.symlinkSync('/usr/lib/npm-library/whatever/1.0.0', path.join(moduleDir, 'whatever'));
     assert.equal(prov.processBundledDeps(path.join(moduleDir, '..')), '');
   });
--- test/test_requires.js
+++ test/test_requires.js
@@ -1,39 +1,41 @@
+/* eslint-env mocha */
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const tmp = require('tmp');
+
 tmp.setGracefulCleanup();
 
 const req = require('../rpm/npmlib.req');
 
 function makeModulesDir() {
-  var tmpdir = tmp.dirSync();
-  var modulesDir = path.join(tmpdir.name, 'node_modules');
+  const tmpdir = tmp.dirSync();
+  const modulesDir = path.join(tmpdir.name, 'node_modules');
   fs.mkdirSync(modulesDir);
   return modulesDir;
 }
 
-describe('auto-requires.processDependency', function() {
-  it('should convert a symlink to a requirement', function() {
-    var modulesDir = makeModulesDir();
+describe('auto-requires.processDependency', () => {
+  it('should convert a symlink to a requirement', () => {
+    const modulesDir = makeModulesDir();
     fs.symlinkSync('/usr/lib/npm-library/test-modules/1.2.3',
       path.join(modulesDir, 'test-module'));
 
     assert.equal(req.processDependency(path.join(modulesDir, '..'), 'test-module'),
-                 'npmlib(test-module) = 1.2.3\n');
+      'npmlib(test-module) = 1.2.3\n');
   });
 
-  it('should throw an error on a missing symlink', function() {
-    var modulesDir = makeModulesDir();
+  it('should throw an error on a missing symlink', () => {
+    const modulesDir = makeModulesDir();
     assert.throws(() => {
       req.processDependency(path.join(modulesDir, '..'), 'test-module');
     });
   });
 });
 
-describe('auto-requires.processJSON', function () {
-  it('should walk all package.json dependencies', function () {
-    var modulesDir = makeModulesDir();
+describe('auto-requires.processJSON', () => {
+  it('should walk all package.json dependencies', () => {
+    const modulesDir = makeModulesDir();
     fs.symlinkSync('/usr/lib/npm-library/test-modules/1.2.3',
       path.join(modulesDir, 'test-module-1'));
     fs.symlinkSync('/usr/lib/npm-library/test-modules/2.4.5',
@@ -41,14 +43,14 @@ describe('auto-requires.processJSON', function () {
 
     const testData = {
       dependencies: {
-        "test-module-1": "^1.0.0",
-        "test-module-2": "^2.0.0"
-      }
+        'test-module-1': '^1.0.0',
+        'test-module-2': '^2.0.0',
+      },
     };
 
-    const testResult = "nodejs(engine)\n" +
-      "npmlib(test-module-1) = 1.2.3\n" +
-      "npmlib(test-module-2) = 2.4.5\n";
+    const testResult = 'nodejs(engine)\n'
+      + 'npmlib(test-module-1) = 1.2.3\n'
+      + 'npmlib(test-module-2) = 2.4.5\n';
 
     assert.equal(req.processPackageJSON(path.join(modulesDir, '..', 'package.json'), testData),
       testResult);
