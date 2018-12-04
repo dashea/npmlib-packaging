a4652b3677dc27015c740795edeeee651d357466
David Shea <dshea@redhat.com>
2018-12-04 13:38:38 -0500

    Gather dependencies of bundled modules
    In the auto-requires script, look for bundled modules and recurse on
    their package.jsons to gather the dependencies of the bundled module.
    This needs to happen here instead of in the fileattrs path regex since
    we still want to skip these files for auto-provides purposes.

--- test/test_requires.js
+++ test/test_requires.js
@@ -15,6 +15,11 @@ function makeModulesDir() {
   return modulesDir;
 }
 
+function uniq(arr) {
+  arr.sort();
+  return arr.filter((a, idx) => !(arr[idx + 1] === a));
+}
+
 describe('auto-requires.processDependency', () => {
   it('should convert a symlink to a requirement', () => {
     const modulesDir = makeModulesDir();
@@ -47,14 +52,58 @@ describe('auto-requires.processJSON', () => {
         'test-module-2': '^2.0.0',
       },
     };
+    const packageJSONPath = path.join(modulesDir, '..', 'package.json');
+    fs.writeFileSync(packageJSONPath, JSON.stringify(testData));
 
     const testResult = [
+      'nodejs(engine)',
       'npmlib(test-module-1) = 1.2.3',
       'npmlib(test-module-2) = 2.4.5',
-      'nodejs(engine)',
     ];
 
-    assert.deepStrictEqual(req.processPackageJSON(path.join(modulesDir, '..', 'package.json'), testData),
-      testResult);
+    assert.deepStrictEqual(uniq(req.processPackageJSON(packageJSONPath)), testResult);
+  });
+
+  it('should walk bundled module dependencies', () => {
+    const modulesDir = makeModulesDir();
+
+    fs.symlinkSync('/usr/lib/npm-library/test-module-1/1.2.3',
+      path.join(modulesDir, 'test-module-1'));
+    fs.symlinkSync('/usr/lib/npm-library/test-module-2/2.4.5',
+      path.join(modulesDir, 'test-module-2'));
+
+    const testData = {
+      dependencies: {
+        'test-module-1': '^1.0.0',
+        'test-module-2': '^2.0.0',
+        'test-module-3': '^3.0.0',
+      },
+    };
+    const packageJSONPath = path.join(modulesDir, '..', 'package.json');
+    fs.writeFileSync(packageJSONPath, JSON.stringify(testData));
+
+    const subModulesDir = path.join(modulesDir, 'test-module-3', 'node_modules');
+    fs.mkdirSync(path.join(modulesDir, 'test-module-3'));
+    fs.mkdirSync(subModulesDir);
+
+    fs.symlinkSync('/usr/lib/npm-library/test-module-4/4.5.6',
+      path.join(subModulesDir, 'test-module-4'));
+
+    const subTestData = {
+      dependencies: {
+        'test-module-4': '^4.0.0',
+      },
+    };
+    const subPackageJSONPath = path.join(modulesDir, 'test-module-3', 'package.json');
+    fs.writeFileSync(subPackageJSONPath, JSON.stringify(subTestData));
+
+    const testResult = [
+      'nodejs(engine)',
+      'npmlib(test-module-1) = 1.2.3',
+      'npmlib(test-module-2) = 2.4.5',
+      'npmlib(test-module-4) = 4.5.6',
+    ];
+
+    assert.deepStrictEqual(uniq(req.processPackageJSON(packageJSONPath)), testResult);
   });
 });
